647306bb6491c3ac232466318757bd8d3f944c90
sonal <sonalamba@codewithsonal.com>
2022-02-05 18:53:04 +0530

    INNER JOIN and LEFT JOIN

--- Joining_Multiple_Tables.sql
+++ Joining_Multiple_Tables.sql
@@ -44,3 +44,64 @@ FROM customer c INNER JOIN payment p ON c.customer_id = p.customer_id
 ORDER BY payment_date DESC;
 
 SELECT e.first_name employee, m.first_name manager FROM staff e INNER JOIN staff m ON e.staff_id = m.staff_id;
+
+-- INNER JOIN Clause
+
+SELECT customer.customer_id, first_name, last_name, amount, payment_date
+FROM customer INNER JOIN payment ON customer.customer_id = payment.customer_id
+ORDER BY payment_date;
+
+SELECT
+	customer.customer_id,
+	first_name,
+	last_name,
+	amount,
+	payment_date
+FROM
+	customer
+INNER JOIN payment 
+    ON payment.customer_id = customer.customer_id
+WHERE first_name = 'Joyce' AND payment_date = CAST('2007-02-14 21:21:59.996577' AS timestamp without time zone)   
+ORDER BY payment_date;
+
+SELECT c.customer_id, first_name, last_name, amount, payment_date
+FROM customer c INNER JOIN payment p ON c.customer_id = p.customer_id
+WHERE c.customer_id = 2;
+
+SELECT customer.customer_id, first_name, last_name, amount, payment_date
+FROM customer INNER JOIN payment USING(customer_id)
+ORDER BY payment_date;
+
+SELECT c.customer_id, c.first_name customer_first_name, c.last_name customer_last_name, s.first_name staff_first_name,
+s.last_name staff_last_name, amount, payment_date
+FROM customer c INNER JOIN payment p ON c.customer_id = p.customer_id INNER JOIN staff s ON s.staff_id = p.staff_id
+ORDER BY payment_date;
+
+SELECT c.customer_id, c.first_name customer_first_name, c.last_name customer_last_name, s.first_name staff_first_name,
+s.last_name staff_last_name, amount, payment_date, st.last_update
+FROM customer c INNER JOIN payment p ON c.customer_id = p.customer_id INNER JOIN staff s ON s.staff_id = p.staff_id INNER JOIN store st ON c.store_id = st.store_id 
+ORDER BY payment_date;
+
+SELECT c.customer_id, c.first_name customer_first_name, c.last_name customer_last_name, s.first_name staff_first_name, amount, payment_date, st.last_update
+FROM customer c INNER JOIN payment p ON c.customer_id = p.customer_id INNER JOIN staff s ON s.staff_id = p.staff_id INNER JOIN store st ON c.store_id = st.store_id
+WHERE c.customer_id = 416;
+
+SELECT p.customer_id, first_name staff_first_name, amount, payment_date
+FROM payment p INNER JOIN staff s ON s.staff_id = p.staff_id
+WHERE p.customer_id = 416;
+
+-- LEFT JOIN Clause
+
+SELECT  f.film_id, title, i.inventory_id
+FROM film f LEFT JOIN inventory i ON f.film_id = i.film_id
+ORDER BY title;
+
+SELECT f.film_id, title, inventory_id, i.film_id
+FROM film f LEFT JOIN inventory i ON f.film_id = i.film_id
+WHERE i.film_id IS NULL
+ORDER BY title;
+
+SELECT f.film_id, title, inventory_id
+FROM film f LEFT JOIN inventory i USING(film_id)
+WHERE i.film_id IS NULL
+ORDER BY title;
