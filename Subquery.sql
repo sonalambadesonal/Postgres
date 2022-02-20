948bf2dee0960c30c3b6b05eded8327289ecdd77
sonal <sonalamba@codewithsonal.com>
2022-02-20 18:20:44 +0530

    ANY, ALL, EXISTS Operators

--- Subquery.sql
+++ Subquery.sql
@@ -30,5 +30,72 @@ SELECT first_name, last_name
 FROM customer
 WHERE EXISTS( SELECT 1 FROM payment WHERE payment.customer_id = customer.customer_id );
 
+-- ANY Operator
+
+SELECT MAX(length), category_id
+FROM film INNER JOIN film_category USING(film_id)
+GROUP BY category_id
+ORDER BY MAX(length);
+
+SELECT title,length
+FROM film
+WHERE length >= ANY(SELECT MAX(length)
+FROM film INNER JOIN film_category USING(film_id)
+GROUP BY category_id
+ORDER BY category_id)
+ORDER BY length;
+
+SELECT title, category_id
+FROM film INNER JOIN film_category USING(film_id)
+WHERE category_id = ANY(SELECT category_id FROM category WHERE name = 'Drama' OR name = 'Action');
+
+
+SELECT * FROM category;
+
+-- ALL Operator
+
+SELECT COUNT(length), rating
+FROM film
+GROUP BY rating
+ORDER BY COUNT(length);
+
+SELECT ROUND(AVG(length), 2) avg_length
+FROM film
+GROUP BY rating
+ORDER BY avg_length DESC;
+
+SELECT film_id, title, length
+FROM film
+WHERE length > ALL(SELECT ROUND(AVG(length), 2) avg_length
+FROM film
+GROUP BY rating)
+ORDER BY length;
+
+SELECT film_id, title, length
+FROM film
+WHERE length < ALL(SELECT ROUND(AVG(length), 3) avg_length FROM film GROUP BY rating);
+
+-- EXSITS Operator
+
+SELECT 1, customer.customer_id,amount
+FROM customer  INNER JOIN payment  USING(customer_id)
+WHERE amount > 11;
+
+SELECT first_name, last_name
+FROM customer c
+WHERE EXISTS(SELECT 1 FROM payment p WHERE c.customer_id = p.customer_id AND p.amount>11);
+
+SELECT first_name, last_name
+FROM customer c
+WHERE NOT EXISTS(SELECT 1 FROM payment p WHERE c.customer_id = p.customer_id AND amount > 11);
+
+SELECT first_name, last_name
+FROM customer c
+WHERE EXISTS(SELECT 1 FROM payment p WHERE c.customer_id = p.customer_id AND amount < 11);
+
+SELECT first_name, last_name
+FROM customer
+WHERE EXISTS(SELECT NULL);
+
 
 
