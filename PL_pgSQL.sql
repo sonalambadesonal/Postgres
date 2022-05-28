0034753a9444bc04da13427fb9f7485dd889142a
sonal <sonalamba@codewithsonal.com>
2022-05-28 15:08:25 +0530

    Variables, copying date types, variable in block and subblock

--- PL_pgSQL.sql
+++ PL_pgSQL.sql
@@ -47,13 +47,79 @@ $$
   where film_id = id;
 $$;
 
+-- block structure
+do $$
+ <<first_block>>
+declare
+	film_count integer := 0;
+begin
+	-- get the number of films
+	select count(*)
+	into film_count
+	from film;
+	-- display a message
+	raise notice 'The number of films is %', film_count;
+end first_block $$;
 
+-- Variables
 
+do $$
+declare
+	counter integer := 1;
+	first_name varchar(50) := 'John';
+	last_name varchar(50) := 'Doe';
+	payment numeric(11,2) := 20.5;
+begin 
+	raise notice '% % % has been paid % USD',
+		counter,first_name, last_name, payment;
+end $$;
+
+do $$
+declare
+	created_at time := now();
+begin
+ 	raise notice '%', created_at;
+	perform pg_sleep(10);
+	raise notice '%', created_at;
+end $$;
    
+-- Copying Data Tyoes
 
+do $$
+declare
+	film_title film.title%type;
+	featured_title film_title%type;
+begin
+	-- get title of the film id 100
+	select title
+	from film 
+	into film_title
+	where film_id = 100;
+	
+	-- show the film title
+	raise notice 'Film title id 100: %s', film_title;
+end $$;
 
+-- Variables in block and subblock
 
-
+do $$
+	<<outer_block>>
+declare
+	counter integer := 0;
+begin
+	counter := counter + 1;
+	raise notice 'The current value of the counter is %', counter;
+	
+	declare
+		counter integer := 0;
+	begin 
+		counter := counter + 10;
+		raise notice 'Counter Value in the subblock is %', counter;
+		raise notice 'Counter Value in Outer Block is %', outer_block.counter;
+	end;
+	
+	raise notice 'Counter value in Outer block is %', counter;
+end outer_block $$;
 
 
 
