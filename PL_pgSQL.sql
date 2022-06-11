8ec2a51db443437b55b69b2373c10d147ce00b91
sonal <sonalamba@codewithsonal.com>
2022-06-11 15:00:19 +0530

    while loop, for loop

--- PL_pgSQL.sql
+++ PL_pgSQL.sql
@@ -393,6 +393,94 @@ begin
 	raise notice '%', fib;
 end $$;
 
+-- While Loop
+
+do $$
+declare
+	counter integer := 0;
+begin
+	while counter < 5 loop
+		raise notice 'Counter %', counter;
+			counter := counter + 1;
+	end loop;
+end $$;
+
+
+-- For Loop
+do $$
+begin
+	for counter in 1..5 loop
+		raise notice 'Counter: %', counter;
+	end loop;
+end $$;
+
+do $$
+begin
+	for counter in reverse 5..1 loop
+		raise notice  'Counter : %', counter;
+	end loop;
+end $$;
+
+do $$
+begin
+	for counter in 1..6 by 2 loop
+		raise notice 'Counter : %', counter;
+	end loop;
+end $$;
+
+do $$
+begin
+	for counter in reverse 6..1 by 2 loop
+		raise notice 'Counter : %', counter;
+	end loop;
+end $$;
+
+-- for loop iterate over result set.
+
+do $$
+declare
+	f record;
+begin
+	for f in select title, length
+			from film
+			order by length desc, title
+			limit 10
+	loop
+		raise notice '%(% mins)', f.title, f.length;
+	end loop;
+end $$;
+
+-- for loop iterate over result set of a dynamic query
+-- 1.select title, release_year from film order by title limit 1;
+--2. select title, release_year from film order by release_year limit 1;
+
+do $$
+declare
+	-- sort by 1:title, 2:release year
+	sort_type smallint  := 1;
+	-- return the number of films
+	rec_count int := 10;
+	-- use the iterate over the film
+	rec record;
+	-- dynamic query
+	query text;
+begin
+	query := 'select title, release_year from film';
+	
+	if sort_type = 1 then
+		query := query || ' order by title';
+	elseif sort_type = 2 then
+		query := query ||  ' order by release_year';
+	else
+		raise 'invalid sort type %s', sort_type;
+	end if;
+	
+	query := query || ' Limit $1';
+	
+	for rec in execute query using rec_count loop
+		raise notice '% - %', rec.release_year, rec.title;
+	end loop;
+end $$;
 
 
 
