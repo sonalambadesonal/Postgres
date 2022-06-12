0cbe8ddde69e789515e1c99001dda010e99e1bef
sonal <sonalamba@codewithsonal.com>
2022-06-12 14:07:17 +0530

    Exit, Continue

--- PL_pgSQL.sql
+++ PL_pgSQL.sql
@@ -482,7 +482,75 @@ begin
 	end loop;
 end $$;
 
+--Exit Statment
 
+do $$
+declare
+	i int := 0;
+	j int := 0;
+begin
+	<<outer_loop>>
+	loop
+		i = i + 1;
+		exit  when i > 3;
+			-- inner loop
+			j = 0;
+			<<inner_loop>>
+			loop
+				j = j + 1;
+				exit when j > 3;
+				raise notice '(i,j): % %', i,j;
+			end loop inner_loop;
+	end loop outer_loop;
+end $$;
+
+do $$
+declare
+	i int := 0;
+	j int := 0;
+begin
+	<<outer_loop>>
+	loop
+		i = i + 1;
+		exit  when i > 3;
+			-- inner loop
+			j := 0;
+			<<inner_loop>>
+			loop
+				j = j + i;
+				exit inner_loop when j > i;
+				raise notice '(i,j): (% %)', i,j;
+			end loop inner_loop;
+	end loop outer_loop;
+end $$;
+	
+do $$
+begin
+	<<simple_block>>
+	begin
+		exit simple_block;
+		-- for demo purposes
+		raise notice '%', 'Unreachable!';
+	end;
+	raise notice '%', 'End of block';
+end $$;
+
+--Continue
+
+do $$
+declare
+	counter int := 0;
+begin
+	loop
+		counter = counter + 1;
+		-- exit the loop when counter > 10
+		exit when counter > 10;
+		-- skip the current iteration if counter is an even number.
+		continue when mod(counter,2) = 0;
+		-- print out the counter,
+		raise notice '%', counter;
+	end loop;
+end $$;
 
 
 
