0003df6cfff9aa1fc83a44e4922d44fc84cac94c
sonal <sonalamba@codewithsonal.com>
2022-06-26 14:06:15 +0530

    Function Overloading

--- PL_pgSQL.sql
+++ PL_pgSQL.sql
@@ -643,7 +643,74 @@ end $$;
 
 select swap(10,20);
 
+-- Function Overloading
 
+create or replace function get_rental_duration(
+	p_customer_id integer
+)
+returns integer
+language plpgsql
+as $$
+declare
+	rental_duration integer;
+begin
+	select
+		sum(extract(day from return_date - rental_date))
+	into rental_duration
+	from rental
+	where customer_id = p_customer_id;
+	return rental_duration;
+end $$;
+
+select * from get_rental_duration(509);
+
+select * from rental;
+
+-----
+
+create or replace function get_rental_duration(
+	p_customer_id integer,
+	p_from_date date
+)
+
+returns integer
+language plpgsql
+as $$
+declare
+	rental_duration integer;
+begin
+	-- get the rental duration based on customer_id
+	-- and rental date
+	
+	select sum(extract(day from return_date + '12:00:00' - rental_date))
+	into rental_duration
+	from rental
+	where customer_id = p_customer_id and rental_date >= p_from_date;
+	return rental_duration;
+end $$;
+
+select get_rental_duration(232, '2005-07-01');
+	
+-- function overloading and defualt parameter
+
+create or replace function get_rental_duration(
+	p_customer_id integer,
+	p_from_date date default '2005-01-01'
+)
+returns integer
+language plpgsql
+as $$
+declare
+	rental_duration integer;
+begin
+	select sum(extract(day from return_date + '12:00:00' - rental_date))
+	into rental_duration
+	from rental
+	where customer_id = p_customer_id and rental_date >= p_from_date;
+	return rental_duration;
+end $$;
+
+select get_rental_duration(232,'2005-07-01');
 	
 
 
